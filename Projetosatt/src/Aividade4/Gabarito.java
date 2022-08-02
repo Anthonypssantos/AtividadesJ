72af8e48b18a64e57bf2c731fe70d5022ff718c9
Anthonypssantos <104658705+Anthonypssantos@users.noreply.github.com>
2022-08-01 21:40:28 -0300

    Javao

---
+++ Projetosatt/src/Aividade4/Gabarito.java
@@ -0,0 +1,13 @@
+package Aividade4;
+
+import javax.swing.JOptionPane;
+
+public class Gabarito {
+	
+	public void gabarit() {
+		
+		JOptionPane.showMessageDialog(null, "GABARITOS\n==========\nQuestão 1: A"+"\nQuestão 2: A"+"\nQuestão 3: A"+"\nQuestão 4: C"+"\nQuestão 5: A"+"\nQuestão 6: C"+"\nQuestão 7: A"+"\nQuestão 8: B"+"\nQuestão 9: C"+"\nQuestão 10: C"+"\nQuestão 11: C"+"\nQuestão 12: C"+"\nQuestão 13: C"+"\nQuestão 14: C"+"\nQuestão 15: C");	
+		
+	}
+	
+}
--- Projetosatt/src/Aividade4/prova.java
+++ Projetosatt/src/Aividade4/prova.java
@@ -5,6 +5,7 @@ import javax.swing.JOptionPane;
 public class prova extends Provareturna{
 	
 	Provareturna pvrta = new Provareturna();
+	Gabarito gab = new Gabarito();
 	
 	String questao=" ";
 	double nota=0.0, l=0.0;
@@ -24,8 +25,7 @@ public class prova extends Provareturna{
 
 	public void gabarito() {		
 		
-		JOptionPane.showMessageDialog(null, "GABARITOS\n==========\nQuestão 1: A"+"\nQuestão 2: A"+"\nQuestão 3: A"+"\nQuestão 4: C"+"\nQuestão 5: A"+"\nQuestão 6: C"+"\nQuestão 7: A"+"\nQuestão 8: B"+"\nQuestão 9: C"+"\nQuestão 10: C"+"\nQuestão 11: C"+"\nQuestão 12: C"+"\nQuestão 13: C"+"\nQuestão 14: C"+"\nQuestão 15: C");
-		
+		gab.gabarit();
 	
 	}
 	
